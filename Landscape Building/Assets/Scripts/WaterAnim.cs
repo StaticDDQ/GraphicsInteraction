916f6088efd0ad79a6557fd9b06755d13718b20c
StaticDDQ <31177227+StaticDDQ@users.noreply.github.com>
2018-08-15 16:19:05 +1000

    Attempting to add water effect

---
+++ Landscape Building/Assets/Scripts/WaterAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterAnim : MonoBehaviour
+{
+
+    private new Renderer renderer;
+
+    private Vector2 uvOffset = Vector2.zero;
+    public Vector2 animRate = new Vector2(1.0f, 0.0f);
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        uvOffset += (animRate * Time.deltaTime);
+        renderer.materials[0].SetTextureOffset("_MainTex", uvOffset);
+    }
+}
